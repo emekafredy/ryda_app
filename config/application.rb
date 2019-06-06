73b8140ebb439c973a00a6985a07b99d6b06e728
emekafredy <chinedufrderick@gmail.com>
2019-06-06 10:56:56 +0100

    deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require "dotenv"
-# require "rails/test_unit/railtie"
-Dotenv.load("./.env")
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
